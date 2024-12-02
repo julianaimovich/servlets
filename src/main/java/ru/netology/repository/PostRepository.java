d822f3ffb03a68a81373170787d52b56fdfb0094
najmovich <najmovich@protei.ru>
2024-12-02 08:57:27 +0300

    Annotation config added

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -2,38 +2,15 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-public class PostRepository {
+public interface PostRepository {
+    List<Post> all();
 
-    private final List<Post> posts = new ArrayList<>();
+    Optional<Post> getById(long id);
 
-    public List<Post> all() {
-        return posts;
-    }
+    Post save(Post post);
 
-    public Optional<Post> getById(long id) {
-        return posts.stream().filter(x -> x.getId() == id).findFirst();
-    }
-
-    public Post save(Post post) {
-        if (post.getId() == 0) {
-            post.setId(posts.size() + 1);
-            posts.add(post);
-        } else {
-            List<Long> ids = posts.stream().map(Post::getId).collect(Collectors.toList());
-            if (ids.contains(post.getId())) {
-                int postIndex = posts.indexOf(getById(post.getId()).orElseThrow());
-                posts.set(postIndex, post);
-            }
-        }
-        return post;
-    }
-
-    public void removeById(long id) {
-        posts.removeIf(post -> post.getId() == id);
-    }
+    void removeById(long id);
 }
